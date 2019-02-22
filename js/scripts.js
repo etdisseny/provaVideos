904431b3c3e6b837c4f1c132b53ec6640bf27054
Ester Termens <estertermens@hotmail.com>
2019-02-22 13:09:49 +0100

    creació formulari

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var formIti= document.getElementById("formItinerari");
+
+var itinerari= formIti.itinerari;
+
+function seleccionar () {
+	if (itinerari == fe){
+		document.getElementById("front").style.display ="block";
+	}
+}
+
